874bc9028c60ce2348c3c44e934152257e834947
federicodececco <federicodececco1@gmail.com>
2024-12-04 18:27:42 +0100

    add: createRandomNumber func

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ function isEven(x, y) {
     else return NaN
 
 }
+function createRandomNumber(min, max) {
+    if (typeof (min) !== 'number' || typeof (max) !== 'number' || ) {
+        return NaN
+    }
+    else
+        if (min > max) {
+            return Math.floor(Math.random() * (min - max) + max)
 
-
+        }
+        else return Math.floor(Math.random() * (max - min) + min)
+}
 
 
