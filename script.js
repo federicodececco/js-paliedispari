ef79d276041616d64dd81bbbb22116529b340943
federicodececco <federicodececco1@gmail.com>
2024-12-04 18:19:59 +0100

    add: isEven func

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
 
-let userNumber
-while ()
-    userNumber = prompt("Inserisci un numero")
+
+function isEven(x, y) {
+    if (typeof (x) === 'number' || typeof (y) === 'number') {
+        if ((x + y) % 2 === 0) {
+            return true
+        }
+        else return false
+    }
+    else return NaN
+
+}
+
+
+
+
