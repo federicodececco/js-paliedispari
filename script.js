7de94eb65dd0d033d1ca6a6d81656ed523664f4c
federicodececco <federicodececco1@gmail.com>
2024-12-04 18:39:07 +0100

    add: pari e dispari

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 function isEven(x, y) {
-    if (typeof (x) === 'number' || typeof (y) === 'number') {
+    if (typeof (x) === 'number' && typeof (y) === 'number') {
         if ((x + y) % 2 === 0) {
             return true
         }
@@ -12,7 +12,7 @@ function isEven(x, y) {
 
 }
 function createRandomNumber(min, max) {
-    if (typeof (min) !== 'number' || typeof (max) !== 'number' || ) {
+    if (typeof (min) !== 'number' || typeof (max) !== 'number'  ) {
         return NaN
     }
     else
@@ -21,6 +21,31 @@ function createRandomNumber(min, max) {
 
         }
         else return Math.floor(Math.random() * (max - min) + min)
+}
+
+
+let userChoice
+let userNumber
+
+do {
+    userChoice = prompt("Scegli: pari o dispari?")
+} while (userChoice !== "pari" && userChoice !== "dispari")
+
+do {
+    userNumber = prompt("Scegli un numero da 1 a 5")
+} while (typeof (userNumber) !== 'number' || userNumber > 5 || userNumber < 1)
+let computerNumber = createRandomNumber(1, 5)
+if (userChoice === 'pari' && (isEven(userNumber, computerNumber))) {
+    alert("Hai vinto!")
+
+}
+else if (userChoice === 'dispari' && !(isEven(userNumber, computerNumber))) {
+    alert("Hai vinto!")
+
+}
+else {
+    alert("Hai perso!")
+
 }
 
 
