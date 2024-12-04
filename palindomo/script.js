3f17e58f1c5552d06eba36e6d1a1273efd455308
federicodececco <federicodececco1@gmail.com>
2024-12-04 18:57:06 +0100

    palindromo

--- palindomo/script.js
+++ palindomo/script.js
@@ -1,1 +1,18 @@
 
+function palindromeCheck(word) {
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== word[word.length - i - 1]) {
+            return false
+        }
+    }
+    return true
+}
+
+let userInput = prompt("Scrivi una parola")
+if (palindromeCheck(userInput)) {
+    alert("La parola è palindrma!")
+}
+else alert("La parola non è palindroma")
+
+
